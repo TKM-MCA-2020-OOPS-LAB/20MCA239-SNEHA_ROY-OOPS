e138ac601e2c5d8580b6b0538859697c5cf9b656
26sneharoy <snehathankomroy@gmail.com>
2021-05-06 13:22:32 +0530

    06-05-2021

---
+++ CO2_q1.java
@@ -0,0 +1,18 @@
+package product;
+import java.util.Arrays;
+
+public class CO2_q1 {
+
+
+	
+	public static void main(String[] args) {
+		
+		String[] words= {"albin","vishnu","blessy","lakshmi","ann"};
+		Arrays.sort(words);
+		System.out.println(Arrays.toString(words));
+		 
+	
+	}
+}
+
+
