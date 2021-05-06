cadb9dd961f07b1b3fb0f07aee170899ddab98a3
26sneharoy <snehathankomroy@gmail.com>
2021-05-06 20:21:07 +0530

    06-05-2021

---
+++ CO2_q2.java
@@ -0,0 +1,17 @@
+package product;
+
+public class CO2_q2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] words= {"Akshai","nancy","jithya","devika","sabeel"};
+		for(int i = 0; i < words.length; i ++) {
+			if(words[i]== "Akshai") {
+				System.out.println(words[i]+" is in the index position :"+i);
+			}
+			
+		}
+
+	}
+
+}
