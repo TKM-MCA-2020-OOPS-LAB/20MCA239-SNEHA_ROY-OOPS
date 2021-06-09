ff4940b9d8461edd1bd53d733c7b8e34c1467255
26sneharoy <74782706+26sneharoy@users.noreply.github.com>
2021-06-09 23:26:16 +0530

    Co4_q3 (09-06-2021)

---
+++ UsernameException.java
@@ -0,0 +1,27 @@
+package product;
+
+public class UsernameException extends Exception {
+	
+	int username;
+
+	public UsernameException(int Username) {
+		super();
+		this.username= Username;
+	}
+	public String information() {
+		
+		switch (username) {
+		
+		case 1 :
+			return("invalid username the username should not contain space");
+			
+		case 2 :
+			return ("invalid username the length of the username should be 6 \n");
+			
+		}
+		return null;
+		
+	}
+	
+
+}
