04d046f3d1490ff4955e80be2c9d55b6440e9417
26sneharoy <74782706+26sneharoy@users.noreply.github.com>
2021-08-30 18:30:45 +0530

    co4_q11

---
+++ CO4_q11.java
@@ -0,0 +1,25 @@
+package product;
+import java.util.LinkedList;
+
+public class CO4_q11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		LinkedList<String> linkedlist = new LinkedList<String>();
+		
+		linkedlist.add("ANN");
+		linkedlist.add("MEGHA");
+		linkedlist.add("GAUTHAMI");
+		linkedlist.add("MANNA");
+		
+		
+		System.out.println("original linkedlist : "+linkedlist);
+		linkedlist.clear();
+		
+		System.out.println("after clearing elements from linkedlist: "+linkedlist);
+
+	}
+
+}
