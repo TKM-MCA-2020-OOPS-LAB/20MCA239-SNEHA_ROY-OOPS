1b5e528fdb13820b96633f730a8d4279871a5d0c
26sneharoy <74782706+26sneharoy@users.noreply.github.com>
2021-09-06 15:34:59 +0530

    06-09-2021  (co5_q1)

---
+++ Five_q1.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+import java.applet.*;
+
+public class Five_q1 extends Applet{
+public void paint(Graphics g){
+g.setColor(Color.magenta);
+g.drawLine(20,30,20,300);
+g.drawRect(70,100,30,30);}}
